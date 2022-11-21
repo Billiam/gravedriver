92a482e83163dfb8094d8234811e0eb7bbecde52
Billiam <billiamthesecond@gmail.com>
2022-11-20 18:53:21 -0600

    Change analog read library, integrate menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
 #include <Arduino.h>
 
-#include <Bounce2.h>
-#include <RBD_Timer.h>
-#include <ResponsiveAnalogRead.h>
-
 #define I2C_PIN_SDA (4u)
 #define I2C_PIN_SCL (5u)
 #define SOLENOID_PIN (18u)
 
+#define UP_PIN (3u)
 #define RIGHT_PIN (0u)
-#define UP_PIN (1u)
+#define DOWN_PIN (1u)
+#define LEFT_PIN (2u)
 
 #define POT_PIN A0
 #define PEDAL_PIN A2
@@ -17,31 +15,29 @@
 #define MIN_POWER 64
 #define REVERSE_PEDAL true
 
-#define MIN_DURATION 5
+#define MIN_DURATION 15
 #define MAX_DURATION 40
-
-#include "solenoid.h"
 
 #include "fast_math.h"
 #include "font_8x6.h"
+#include "graver_menu.h"
+#include "scene.h"
+#include "solenoid.h"
+#include "state.h"
+#include "text_display.h"
+#include <Bounce2.h>
+#include <MenuSystem.h>
+#include <RBD_Timer.h>
+#include <ResponsiveAnalogRead.h>
+#include <cmath>
 #include <hardware/i2c.h>
 #include <map>
 #include <pico/multicore.h>
 #include <shapeRenderer/ShapeRenderer.h>
 #include <ssd1306.h>
 #include <textRenderer/TextRenderer.h>
-
-#include <cmath>
-
 RBD::Timer logger;
 Solenoid solenoid = Solenoid(SOLENOID_PIN);
-
-unsigned int solenoidDuration = MIN_DURATION;
-int pedalMin;
-int pedalMax;
-bool pedalRead = false;
-
-int currentCurve = 0;
 
 ResponsiveAnalogRead pedalInput;
 
@@ -51,7 +47,23 @@ Bounce2::Button upButton;
 Bounce2::Button downButton;
 Bounce2::Button leftButton;
 
-int solUpdate;
+stateType state;
+
+/* Issue:
+menu needs to be global, because it will be accessed from display and main
+loop
+
+
+Menu needs initializing with renderer
+  renderer needs global display
+Menu items need global actions
+  actions need state
+    state must be global
+
+*/
+
+pico_ssd1306::SSD1306 *display;
+TextDisplay *textDisplay;
 
 // TODO: Configurable screen dimensions
 // TODO: Cap duration to frequency percentage or waveTime -
@@ -63,15 +75,15 @@ void plotCurve(pico_ssd1306::SSD1306 *ssd1306, int frequency, int x, int y,
   drawRect(ssd1306, x, y, x + width, startY);
 
   double curve;
-  if (currentCurve == 0) {
+  if (state.curve == 0) {
     curve = 1.0;
   } else {
-    curve = abs(currentCurve);
+    curve = abs(state.curve);
   }
 
-  if (currentCurve == 0) {
+  if (state.curve == 0) {
     drawLine(ssd1306, x, startY, x + width, y);
-  } else if (currentCurve > 0) {
+  } else if (state.curve > 0) {
     for (int xi = 0; xi < width; xi++) {
       double xPct = (1.0 * xi) / width;
       double yOut = fastPrecisePow(xPct, curve);
@@ -89,12 +101,12 @@ void plotCurve(pico_ssd1306::SSD1306 *ssd1306, int frequency, int x, int y,
   int indicatorX;
   int indicatorY;
 
-  if (currentCurve == 0) {
+  if (state.curve == 0) {
     indicatorX = x + pct * width;
     indicatorY = startY - pct * width;
   } else {
     indicatorX = x + pct * width;
-    curve = currentCurve > 0 ? curve : 1.0 / curve;
+    curve = state.curve > 0 ? curve : 1.0 / curve;
     indicatorY = startY - fastPrecisePow(pct, curve) * height;
   }
   fillRect(ssd1306, indicatorX - 1, indicatorY - 1, indicatorX + 2,
@@ -109,10 +121,6 @@ void drawWave(pico_ssd1306::SSD1306 *ssd1306, uint8_t y, int frequency, int spm,
   const int maxLen = 40;
 
   int y1 = y + maxHeight;
-
-  char foo[16];
-  snprintf(foo, sizeof foo, "f: %0.2f", spmPercent);
-  drawText(ssd1306, font_8x6, foo, 20, 50);
 
   if (frequency == 0) {
     drawLine(ssd1306, 5, y1, 123, y1);
@@ -149,57 +157,78 @@ void drawMeter(pico_ssd1306::SSD1306 *ssd1306, int x, int y, int width,
 }
 
 absolute_time_t lastLoopStart;
+
+void drawStatus(pico_ssd1306::SSD1306 *ssd1306)
+{
+  int spm = solenoid.spm;
+  int freq = solenoid.freq;
+
+  int solenoidPower = solenoid.pow;
+
+  drawWave(ssd1306, 0, freq, spm, solenoid.spmPercent(), state.duration,
+           solenoidPower);
+
+  // TODO: track cursor
+  // snprintf(buff, sizeof buff, "spm: %d", spm);
+  textDisplay->setCursor(0, 18);
+  drawMeter(ssd1306, 80, textDisplay->getCursorY(), 46, 8, freq / 1024.0);
+  textDisplay->textfln(1, "spm: %d", spm);
+  // drawText(ssd1306, font_8x6, buff, 0, 18);
+
+  drawMeter(ssd1306, 80, textDisplay->getCursorY(), 46, 8,
+            solenoidPower / 1024.0);
+  textDisplay->textfln(1, "pow: %d", solenoidPower);
+
+  drawMeter(ssd1306, 80, textDisplay->getCursorY(), 46, 8,
+            (1.0 * state.duration - MIN_DURATION) /
+                (MAX_DURATION - MIN_DURATION));
+  textDisplay->textfln(1, "dur: %d", state.duration);
+
+  textDisplay->textfln(1, "off: %d", 60000 / spm - state.duration);
+};
+
+void drawMenu(pico_ssd1306::SSD1306 *ssd1306) { menu.display(); }
+void drawCurve(pico_ssd1306::SSD1306 *ssd1306)
+{
+  plotCurve(ssd1306, solenoid.freq, 5, 5, 55, 55);
+}
+
 void displayLoop()
 {
-  pico_ssd1306::SSD1306 display =
+  pico_ssd1306::SSD1306 displayInst =
       pico_ssd1306::SSD1306(i2c0, 0x3C, pico_ssd1306::Size::W128xH64);
-  display.setOrientation(0);
+  display = &displayInst;
+  TextDisplay textDisplayInst = TextDisplay(display);
+  textDisplay = &textDisplayInst;
 
-  display.sendBuffer(); // Send buffer to device and show on screen
+  displayInst.setOrientation(0);
+  displayInst.sendBuffer(); // Send buffer to device and show on screen
 
   while (1) {
     absolute_time_t now = get_absolute_time();
     int dur = absolute_time_diff_us(lastLoopStart, now);
     lastLoopStart = now;
 
-    display.clear();
+    textDisplay->setCursor(0, 0);
+    displayInst.clear();
 
-    char l1_spm[16];
-    int spm = solenoid.spm;
-    int freq = solenoid.freq;
+    switch (state.scene) {
+      case SCENE_STATUS:
+        drawStatus(display);
+        break;
+      case SCENE_MENU:
+        drawMenu(display);
+        break;
+      case SCENE_CURVE:
+        drawCurve(display);
+        break;
+    }
 
-    int solenoidPower = solenoid.pow;
+    // display FPS
+    textDisplay->setCursor(105, 56);
+    textDisplay->textf(1, "%d", 1000000 / dur);
 
-    drawWave(&display, 0, freq, spm, solenoid.spmPercent(), solenoidDuration,
-             solenoidPower);
-
-    // TODO: track cursor
-    snprintf(l1_spm, sizeof l1_spm, "spm: %d", spm);
-    drawText(&display, font_8x6, l1_spm, 0, 18);
-    drawMeter(&display, 80, 18, 46, 8, freq / 1024.0);
-
-    snprintf(l1_spm, sizeof l1_spm, "pow: %d", solenoidPower);
-    drawText(&display, font_8x6, l1_spm, 0, 28);
-    drawMeter(&display, 80, 28, 46, 8, solenoidPower / 1024.0);
-
-    snprintf(l1_spm, sizeof l1_spm, "dur: %d", solenoidDuration);
-    drawText(&display, font_8x6, l1_spm, 0, 38);
-    drawMeter(&display, 80, 38, 46, 8,
-              (1.0 * solenoidDuration - MIN_DURATION) /
-                  (MAX_DURATION - MIN_DURATION));
-
-    snprintf(l1_spm, sizeof l1_spm, "off: %d", 60000 / spm - solenoidDuration);
-    drawText(&display, font_8x6, l1_spm, 0, 48);
-
-    // plotCurve(&display, freq, 5, 5, 55, 55);
-
-    // snprintf(l1_spm, sizeof l1_spm, "%d", solUpdate);
-    // drawText(&display, font_8x6, l1_spm, 105, 40);
-
-    snprintf(l1_spm, sizeof l1_spm, "%d", 1000000 / dur);
-    drawText(&display, font_8x6, l1_spm, 105, 52);
-
-    display.sendBuffer();
+    display->sendBuffer();
   }
 }
 
@@ -207,9 +236,17 @@ void setup()
 {
   Serial.begin(9600);
   pinMode(SOLENOID_PIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
-  rightButton.attach(RIGHT_PIN, INPUT_PULLUP);
+  state.scene = SCENE_STATUS;
+  state.duration = MIN_DURATION;
+  state.pedalRead = false;
+  state.curve = 0;
+
   upButton.attach(UP_PIN, INPUT_PULLUP);
+  rightButton.attach(RIGHT_PIN, INPUT_PULLUP);
+  downButton.attach(DOWN_PIN, INPUT_PULLUP);
+  leftButton.attach(LEFT_PIN, INPUT_PULLUP);
 
   _i2c_init(i2c0, 1000000); // Use i2c port with baud rate of 1Mhz
   // Set pins for I2C operation
@@ -218,18 +255,17 @@ void setup()
   gpio_pull_up(I2C_PIN_SDA);
   gpio_pull_up(I2C_PIN_SCL);
 
-  sleep_ms(500);
-  multicore_launch_core1(displayLoop);
-  sleep_ms(500);
-  char test[16];
-  snprintf(test, sizeof test, "SPM: %d", solenoid.spm);
+  buildMenu();
 
   pedalInput.enableSleep();
   pedalInput.setSnapMultiplier(0.01);
   pedalInput.enableEdgeSnap();
   pedalInput.setActivityThreshold(10.0);
 
-  logger.setTimeout(100);
+  sleep_ms(500);
+  multicore_launch_core1(displayLoop);
+  sleep_ms(500);
+  // logger.setTimeout(100);
 }
 
 int readPedal()
@@ -240,25 +276,25 @@ int readPedal()
   // val = 1024 - val;
   // }
 
-  if (!pedalRead) {
-    pedalRead = true;
-    pedalMin = val;
-    pedalMax = val;
+  if (!state.pedalRead) {
+    state.pedalRead = true;
+    state.pedalMin = val;
+    state.pedalMax = val;
   }
 
-  if (val > pedalMax) {
-    pedalMax = val;
+  if (val > state.pedalMax) {
+    state.pedalMax = val;
   }
-  if (val < pedalMin) {
-    pedalMin = val;
+  if (val < state.pedalMin) {
+    state.pedalMin = val;
   }
 
-  int range = pedalMax - pedalMin;
+  int range = state.pedalMax - state.pedalMin;
   if (range < 20) {
     return 0;
   }
-  int minThreshold = (int)pedalMin + range * 0.15;
-  int maxThreshold = (int)pedalMin + range * 0.9;
+  int minThreshold = state.pedalMin + range * 0.15;
+  int maxThreshold = state.pedalMin + range * 0.9;
   // Serial.print(pedalMin);
   // Serial.print("\t");
   // Serial.print(minThreshold);
@@ -286,52 +322,118 @@ int readPot()
   return constrain(map(val, 20, 975, 0, 1023), 0, 1023);
 }
 
-int readDuration()
-{
-  leftButton.update();
-  return leftButton.fell();
-}
-
-int readCurve()
+void updateButtons()
 {
   upButton.update();
-  return upButton.fell();
+  rightButton.update();
+  downButton.update();
+  leftButton.update();
 }
 
-void loop()
+void statusLoop()
+{
+  updateButtons();
+
+  if (upButton.fell() || downButton.fell() || leftButton.fell() ||
+      rightButton.fell()) {
+
+    Serial.println("Changing to menu");
+    state.scene = SCENE_MENU;
+  }
+}
+
+void menuLoop()
+{
+  updateButtons();
+
+  if (downButton.fell()) {
+    Serial.println("next");
+    menu.next(true);
+  }
+  if (upButton.fell()) {
+    Serial.println("prev");
+    menu.prev(true);
+  }
+  if (rightButton.fell()) {
+    Serial.println("Select");
+    menu.select();
+  }
+  if (leftButton.fell()) {
+    Serial.println("Back");
+    const Menu *current_menu = menu.get_current_menu();
+    bool skipBack = false;
+
+    if (current_menu != nullptr) {
+      const MenuComponent *current_component =
+          current_menu->get_current_component();
+      if (current_component != nullptr) {
+        if (current_component->has_focus()) {
+          skipBack = true;
+          menu.select();
+        }
+      }
+    }
+
+    if (!skipBack && !menu.back()) {
+      state.scene = SCENE_STATUS;
+    }
+  }
+}
+
+void curveLoop()
+{
+  updateButtons();
+
+  if (upButton.fell() || downButton.fell()) {
+    int dir = downButton.fell() ? 1 : -1;
+    int nextCurve = state.curve + dir;
+    nextCurve = (nextCurve + 19) % 13 - 6;
+    if (abs(nextCurve) == 1) {
+      nextCurve += dir;
+    }
+    Serial.println(nextCurve);
+    state.curve = nextCurve;
+  }
+
+  if (rightButton.fell() || leftButton.fell()) {
+    Serial.write("Back to menu");
+    state.scene = SCENE_MENU;
+  }
+}
+
+void updateSolenoid()
 {
   int pedal = readPedal();
   int pot = readPot();
-  int solStart = micros();
-  solenoid.update(pedal, pot, solenoidDuration, currentCurve);
-  solUpdate = micros() - solStart;
+  solenoid.update(pedal, pot, state.duration, state.curve);
+}
 
-  if (readDuration()) {
-    int nextDuration =
-        solenoidDuration + (solenoidDuration < MIN_DURATION + 15 ? 1 : 5);
-    if (nextDuration > MAX_DURATION) {
-      nextDuration = MIN_DURATION;
-    }
-    solenoidDuration = nextDuration;
+// TODO: Could change a pointer to a different scene instead
+void loop()
+{
+  updateSolenoid();
+
+  switch (state.scene) {
+    case SCENE_STATUS:
+      // code block
+      statusLoop();
+      break;
+    case SCENE_MENU:
+      menuLoop();
+      // code block
+      break;
+    case SCENE_CURVE:
+      curveLoop();
+      break;
   }
 
-  if (readCurve()) {
-    int nextCurve = currentCurve + 1;
-    if (nextCurve > 6) {
-      nextCurve = -6;
-    }
-    if (abs(nextCurve) == 1) {
-      nextCurve++;
-    }
-    currentCurve = nextCurve;
-  }
-  // updateDisplay(pedal, pot, solenoidDuration, solenoid.spm);
+  // updateDisplay(pedal, pot, state.duration, solenoid.spm);
   // if (logger.onRestart()) {
   //   Serial.println(pedal);
   //   Serial.print("\t");
   //   Serial.print(pot);
   //   Serial.print("\t");
-  //   Serial.print(solenoidDuration);
+  //   Serial.print(state.duration);
   //   Serial.print("\t");
   //   Serial.println(solenoid.spm);
   //   Serial.println(micros() - t);
