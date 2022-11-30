d12a035b80c6b9664a484f7bf5a5407cca1aa003
Billiam <billiamthesecond@gmail.com>
2022-11-30 00:40:36 -0600

    Use uint8_t as underlying type

--- src/pedal_mode.h
+++ src/pedal_mode.h
@@ -3,7 +3,8 @@
 
 #include <map>
 
-enum class PedalMode : const unsigned char { FREQUENCY = 0, POWER = 1 };
+enum class PedalMode : uint8_t { FREQUENCY = 0,
+                                 POWER = 1 };
 
 const std::map<PedalMode, const char *> PedalModeLabel = {
     {PedalMode::FREQUENCY, "frequency"}, {PedalMode::POWER, "power"}};
--- src/power_mode.h
+++ src/power_mode.h
@@ -1,6 +1,7 @@
 #ifndef power_mode_h_
 #define power_mode_h_
 
-enum class PowerMode : const unsigned char { POWER = 0, DURATION = 1 };
+enum class PowerMode : const uint8_t { POWER = 0,
+                                       DURATION = 1 };
 
 #endif
