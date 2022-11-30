d7eb0ca88c31ad851e386e80e287783c96887b41
Billiam <billiamthesecond@gmail.com>
2022-11-30 00:38:57 -0600

    Add memory read/write and memory locations

---
+++ src/store.cpp
@@ -0,0 +1,146 @@
+#include "store.h"
+#include <Adafruit_FRAM_SPI.h>
+
+Store::Store(Adafruit_FRAM_SPI *fram) : _fram(fram){};
+
+uint8_t Store::readOffset(uint32_t position, uint8_t size)
+{
+  if (_fram->read8(position) > 0) {
+    return size + 1;
+  } else {
+    return 1;
+  }
+}
+
+uint8_t Store::nextOffset(uint32_t position, uint8_t size)
+{
+  uint8_t read = _fram->read8(position);
+  Serial.print("Read offset: ");
+  Serial.print(position);
+  Serial.print("\t");
+  Serial.println(read);
+  if (read > 0) {
+    return 1;
+  } else {
+    return size + 1;
+  }
+}
+
+uint32_t Store::graverOffset(uint8_t graver, FramKey key)
+{
+  return graver * GRAVER_SIZE + GRAVER_START + GraverAddr.at(key);
+}
+
+void Store::setReadLocation(uint32_t position, uint8_t offset)
+{
+  _fram->writeEnable(true);
+  _fram->write8(position, offset == 1 ? 0 : 1);
+  _fram->writeEnable(false);
+}
+
+uint8_t Store::readUint(uint8_t graver, FramKey key)
+{
+  return readUint(graverOffset(graver, key));
+}
+uint8_t Store::readUint(FramKey key)
+{
+  return readUint(FramAddr.at(key));
+}
+uint8_t Store::readUint(uint32_t position)
+{
+  return _fram->read8(readOffset(position, 1) + position);
+}
+
+uint16_t Store::readUint16(uint8_t graver, FramKey key)
+{
+  return readUint16(graverOffset(graver, key));
+}
+uint16_t Store::readUint16(FramKey key)
+{
+  return readUint16(FramAddr.at(key));
+}
+uint16_t Store::readUint16(uint32_t position)
+{
+  const uint32_t pos = readOffset(position, 2) + position;
+
+  uint16_t out;
+  uint8_t *outPtr = (uint8_t *)&out;
+  *outPtr = _fram->read8(pos);
+  outPtr++;
+  *outPtr = _fram->read8(pos + 1);
+  return out;
+}
+
+void Store::readChars(uint8_t graver, FramKey key, char *result, uint8_t count)
+{
+  readChars(graverOffset(graver, key), result, count);
+}
+void Store::readChars(FramKey key, char *result, uint8_t count)
+{
+  readChars(FramAddr.at(key), result, count);
+}
+void Store::readChars(uint32_t position, char *result, uint8_t count)
+{
+  const uint32_t pos = readOffset(position, count) + position;
+
+  for (uint8_t i = 0; i < count; i++) {
+    result[i] = _fram->read8(pos + i);
+  }
+}
+
+void Store::writeUint(uint8_t graver, FramKey key, uint8_t value)
+{
+  writeUint(graverOffset(graver, key), value);
+}
+void Store::writeUint(FramKey key, uint8_t value)
+{
+  writeUint(FramAddr.at(key), value);
+}
+void Store::writeUint(uint32_t position, uint8_t value)
+{
+  const uint8_t offset = nextOffset(position, 1);
+
+  _fram->writeEnable(true);
+  _fram->write8(position + offset, value);
+  _fram->writeEnable(false);
+  setReadLocation(position, offset);
+}
+
+void Store::writeUint16(uint8_t graver, FramKey key, uint16_t value)
+{
+  writeUint16(graverOffset(graver, key), value);
+}
+void Store::writeUint16(FramKey key, uint16_t value)
+{
+  writeUint16(FramAddr.at(key), value);
+}
+void Store::writeUint16(uint32_t position, uint16_t value)
+{
+  const uint8_t offset = nextOffset(position, 2);
+
+  _fram->writeEnable(true);
+  _fram->write(position + offset, (uint8_t *)&value, sizeof(uint16_t));
+  _fram->writeEnable(false);
+
+  setReadLocation(position, offset);
+}
+
+void Store::writeChars(uint8_t graver, FramKey key, const char *value, uint8_t count)
+{
+  writeChars(graverOffset(graver, key), value, count);
+}
+void Store::writeChars(FramKey key, const char *value, uint8_t count)
+{
+  writeChars(FramAddr.at(key), value, count);
+}
+void Store::writeChars(uint32_t position, const char *value, uint8_t count)
+{
+  const uint8_t offset = nextOffset(position, count);
+  // does this work?
+  _fram->writeEnable(true);
+  for (uint8_t i = 0; i < count; i++) {
+    _fram->write8(position + offset + i, value[i]);
+  }
+  _fram->writeEnable(false);
+  setReadLocation(position, offset);
+}
---
+++ src/store.h
@@ -0,0 +1,90 @@
+#ifndef store_h_
+#define store_h_
+
+#include <Adafruit_FRAM_SPI.h>
+#include <Arduino.h>
+#include <map>
+
+#define GRAVER_SIZE 100
+#define GRAVER_START 80
+
+enum class FramKey : const unsigned char {
+  NAME = 1,
+  POWER = 2,
+  POWER_MODE = 3,
+  PEDAL_MODE = 4,
+  CURVE = 5,
+  PEDAL_MIN = 6,
+  PEDAL_MAX = 7,
+  POWER_MIN = 8,
+
+  DURATION_MIN = 9,
+  DURATION_MAX = 10,
+  FREQUENCY_MIN = 11,
+  FREQUENCY_MAX = 12,
+
+  DURATION = 13,
+
+  GRAVER_COUNT = 14,
+  GRAVER = 15
+};
+
+const std::map<FramKey, uint32_t> FramAddr = {
+    {FramKey::GRAVER_COUNT, 0}, // 8
+    {FramKey::GRAVER, 3},       // 8
+    {FramKey::PEDAL_MIN, 6},    // 16
+    {FramKey::PEDAL_MAX, 11},   // 16
+};
+
+const std::map<FramKey, uint32_t> GraverAddr = {
+    {FramKey::NAME, 0},           // 8 char
+    {FramKey::POWER, 17},         // 16
+    {FramKey::POWER_MODE, 22},    // 8
+    {FramKey::PEDAL_MODE, 25},    // 8
+    {FramKey::CURVE, 28},         // 8
+    {FramKey::POWER_MIN, 31},     // 8
+    {FramKey::DURATION_MIN, 34},  // 8
+    {FramKey::DURATION_MAX, 37},  // 8
+    {FramKey::FREQUENCY_MIN, 40}, // 16
+    {FramKey::FREQUENCY_MAX, 45}, // 16
+    {FramKey::DURATION, 50},      // 8
+};
+
+class Store
+{
+public:
+  Store(Adafruit_FRAM_SPI *fram);
+
+  uint8_t readUint(uint8_t graver, FramKey key);
+  uint8_t readUint(FramKey key);
+  uint8_t readUint(uint32_t position);
+
+  uint16_t readUint16(uint8_t graver, FramKey key);
+  uint16_t readUint16(FramKey key);
+  uint16_t readUint16(uint32_t position);
+
+  void readChars(uint8_t graver, FramKey key, char *result, uint8_t count);
+  void readChars(FramKey key, char *result, uint8_t count);
+  void readChars(uint32_t position, char *result, uint8_t count);
+
+  void writeUint(uint8_t graver, FramKey key, uint8_t value);
+  void writeUint(FramKey key, uint8_t value);
+  void writeUint(uint32_t position, uint8_t value);
+
+  void writeChars(uint8_t graver, FramKey key, const char *value, uint8_t count);
+  void writeChars(FramKey key, const char *value, uint8_t count);
+  void writeChars(uint32_t position, const char *value, uint8_t count);
+
+  void writeUint16(uint8_t graver, FramKey key, uint16_t value);
+  void writeUint16(FramKey key, uint16_t value);
+  void writeUint16(uint32_t position, uint16_t value);
+
+protected:
+  Adafruit_FRAM_SPI *_fram;
+
+  void setReadLocation(uint32_t position, uint8_t offset);
+  uint8_t readOffset(uint32_t position, uint8_t size);
+  uint8_t nextOffset(uint32_t position, uint8_t size);
+  uint32_t graverOffset(uint8_t graver, FramKey key);
+};
+#endif
